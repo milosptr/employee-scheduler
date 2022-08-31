facfe91a7b840a2ce26ee662a88356064ba66b0f
milospetrovic <milosp@ultimatemovers.net>
2022-08-31 16:16:05 +0200

    shift name

--- resources/js/components/Calendar/CalendarHeader.jsx
+++ resources/js/components/Calendar/CalendarHeader.jsx
@@ -5,7 +5,7 @@ export default function CalendarHeader() {
   const state = useSelector((state) => state.general)
   const shifts = Array.from(Array(state.shifts).keys())
   const occupations = state.activeOccupation !== null ? state.occupations.filter((o) => o.id === state.activeOccupation) : state.occupations.filter((o) => o.id !== null)
-  const listShifts = occupations.map((o) => shifts.map((s, i) => <td key={i} className="border-l border-gray-400 py-3 font-semibold">{shiftName[i + 1]}</td>))
+  const listShifts = occupations.map((o) => shifts.map((s, i) => <td key={i} className="border-l border-gray-400 py-3 font-semibold">{shiftName[i]}</td>))
   const shiftName = ['I', 'M', 'II']
 
   return (
