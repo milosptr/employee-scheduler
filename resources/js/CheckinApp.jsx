b3911332fa8e92043203db8dff285a888ec73b08
milospetrovic <milosp@ultimatemovers.net>
2023-02-26 11:47:09 +0000

    checkin app bigger font

--- resources/js/CheckinApp.jsx
+++ resources/js/CheckinApp.jsx
@@ -46,41 +46,41 @@ const CheckinApp = () => {
   return (
     <div className="bg-gray-200 min-h-[100vh] p-[2%]">
       <div className="bg-white rounded-lg p-12">
-        <div className="text-3xl font-semibold text-gray-900 mb-10">Prijavljivanje</div>
+        <div className="text-4xl font-semibold text-gray-900 mb-10">Prijavljivanje</div>
         <div>
-          <div className="text-2xl font-semibold text-gray-900 pb-2 border-b border-gray-100 mb-10">Kuhinja</div>
+          <div className="text-3xl font-semibold text-gray-900 pb-2 border-b border-gray-100 mb-10">Kuhinja</div>
           <div className="grid grid-cols-7 gap-5 text-center">
             { kitchenEmployees.map((employee) => (
               <div
                 key={employee.id}
                 className={`
-                  rounded-lg cursor-pointer py-5 text-xl text-gray-800 min-h-[100px]
+                  rounded-lg cursor-pointer py-5 text-2xl text-gray-800 min-h-[100px]
                   ${employee.lastCheckin && employee.lastCheckin.check_out === null ? 'bg-green-500' : ''}
                   ${employee.lastCheckin === null ? 'bg-gray-300' : ''}
                   ${employee.lastCheckin && employee.lastCheckin.check_out ? 'bg-red-500' : ''}`}
                 onClick={() => handleCheckin(employee)}
               >
                 <strong>{employee.name}</strong>
-                <div className="font-mono text-center text-sm">{ printCheckinDate(employee) }{ printCheckoutDate(employee) }</div>
+                <div className="font-mono text-center text-base">{ printCheckinDate(employee) }{ printCheckoutDate(employee) }</div>
               </div>
             ))}
           </div>
         </div>
         <div className="mt-10">
-          <div className="text-2xl font-semibold text-gray-900 pb-2 border-b border-gray-100 mb-10">Šank</div>
+          <div className="text-3xl font-semibold text-gray-900 pb-2 border-b border-gray-100 mb-10">Šank</div>
           <div className="grid grid-cols-7 gap-5 text-center">
             { barEmployees.map((employee) => (
                 <div
                   key={employee.id}
                   className={
-                    `rounded-lg cursor-pointer py-5 text-xl text-gray-800 min-h-[100px]
+                    `rounded-lg cursor-pointer py-5 text-2xl text-gray-800 min-h-[100px]
                     ${employee.lastCheckin && employee.lastCheckin.check_out === null ? 'bg-green-500' : ''}
                     ${employee.lastCheckin === null ? 'bg-gray-300' : ''}
                     ${employee.lastCheckin && employee.lastCheckin.check_out ? 'bg-red-500' : ''}`}
                   onClick={() => handleCheckin(employee)}
                 >
                   <strong>{employee.name}</strong>
-                  <div className="font-mono text-center text-sm">{ printCheckinDate(employee) }{ printCheckoutDate(employee) }</div>
+                  <div className="font-mono text-center text-base">{ printCheckinDate(employee) }{ printCheckoutDate(employee) }</div>
                 </div>
               ))}
             </div>
