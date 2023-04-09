28255bf1631f8d88b3c38d5500c40963eb53c43a
milospetrovic <milosp@ultimatemovers.net>
2023-04-09 22:24:46 +0200

    optimize schedule service

--- app/Services/ScheduleService.php
+++ app/Services/ScheduleService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services;
 
 use Carbon\Carbon;
@@ -15,33 +16,31 @@ class ScheduleService
     public function setPeriod($range)
     {
         $dates = explode(' to ', $range);
-        $this->startDate = $dates[0];
-        $this->endDate = $dates[1];
-        $this->diffInDays =  Carbon::parse($dates[1])->diffInDays(Carbon::parse($dates[0]));
+        $this->startDate = Carbon::parse($dates[0]);
+        $this->endDate = Carbon::parse($dates[1]);
+        $this->diffInDays = $this->endDate->diffInDays($this->startDate);
         return $this;
     }
 
     public function setSchedules()
     {
-        $this->schedules = ScheduleResource::collection(Schedule::with('employee')->whereBetween('date', [$this->startDate, $this->endDate])->get());
+        $this->schedules = Schedule::with('employee')
+            ->whereBetween('date', [$this->startDate->toDateString(), $this->endDate->toDateString()])
+            ->get()
+            ->groupBy('date');
         return $this;
     }
 
     public function parseDays()
     {
         $days = [];
-        $startDate = Carbon::parse($this->startDate);
-
-        // Pre-process schedules and group them by date
-        $groupedSchedules = $this->schedules->groupBy(function ($value) {
-            return Carbon::parse($value->date)->toDateString();
-        });
 
         for ($i = 0; $i <= $this->diffInDays; $i++) {
-            $date = $startDate->copy()->addDays($i);
+            $date = $this->startDate->copy()->addDays($i);
             $dateString = $date->toDateString();
 
-            $schedules = $groupedSchedules->get($dateString, collect([]));
+            $schedules = $this->schedules->get($dateString, collect([]));
+            $schedules = ScheduleResource::collection($schedules);
 
             $days[] = [
                 'date' => $dateString,
