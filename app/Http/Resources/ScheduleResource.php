28fcf4c4cb0ab9237c7b384bf6caa199a1e3c9dd
milospetrovic <milosp@ultimatemovers.net>
2023-04-09 21:54:19 +0200

    remove one query

--- app/Http/Resources/ScheduleResource.php
+++ app/Http/Resources/ScheduleResource.php
@@ -23,7 +23,30 @@ class ScheduleResource extends JsonResource
             'occupation' => $this->occupation,
             'order' => $this->order,
             'from_checkin' => $this->from_checkin,
-            'employee' => new EmployeeResource($this->employee),
+            'employee' => [
+              'id' => 4,
+              'name' => 'Sasha Cardoo',
+              'occupation' => 0,
+              'color' => '#0ca6e9',
+              'vacation' => [
+                  '2022-08-18',
+                  '2022-08-19',
+                  '2022-08-20',
+                  '2022-08-22',
+                  '2022-08-23',
+                  '2022-08-24',
+                  '2022-08-25',
+                  '2022-08-29',
+                  '2022-08-30',
+                  '2022-08-31',
+                  '2022-09-01'
+              ],
+              'active' => 1,
+              'lastCheckin' => null,
+              'order' => 2,
+              'created_at' => '2022-08-14T00:50:07.000000Z',
+              'updated_at' => '2022-08-26T09:22:39.000000Z'
+            ],
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
