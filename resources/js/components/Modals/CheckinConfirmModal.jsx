61f4077acf977df3be4556767135ede652b29c40
milospetrovic <milosp@ultimatemovers.net>
2023-04-09 11:43:49 +0200

    calculate checkin checkout time

--- resources/js/components/Modals/CheckinConfirmModal.jsx
+++ resources/js/components/Modals/CheckinConfirmModal.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Fragment } from 'react'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon, XMarkIcon, ExclamationTriangleIcon} from '@heroicons/react/24/outline'
+import { CheckIcon, XMarkIcon, ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 import { setActiveEmployee, setEmployees } from '../../store/general'
 const CHECK_IN = 1
 const CHECK_OUT = 2
@@ -14,165 +14,192 @@ export const CheckinConfirmModal = () => {
   const [processing, setProcessing] = useState(false)
   const dispatch = useDispatch()
   const activeEmployee = useSelector((state) => state.general.activeEmployee)
-  const checkinType = activeEmployee.lastCheckin ? (activeEmployee.lastCheckin.check_out ? CHECK_IN : CHECK_OUT) : CHECK_IN
+  const checkinType = activeEmployee.lastCheckin
+    ? activeEmployee.lastCheckin.check_out
+      ? CHECK_IN
+      : CHECK_OUT
+    : CHECK_IN
   const lastCheckinId = activeEmployee.lastCheckin ? activeEmployee.lastCheckin.id : null
   const handleClose = () => {
-    if(!processing)
-      dispatch(setActiveEmployee(null))
+    if (!processing) dispatch(setActiveEmployee(null))
   }
   const handleCheckin = () => {
-    if(loading && processing) return
+    if (loading && processing) return
+    console.time('checkin')
     setLoading(true)
     setProcessing(true)
-    axios.post(`/api/employees/${activeEmployee.id}/checkin`, { employee: activeEmployee.id, checkin: lastCheckinId})
+    axios
+      .post(`/api/employees/${activeEmployee.id}/checkin`, { employee: activeEmployee.id, checkin: lastCheckinId })
       .then((response) => {
         dispatch(setEmployees(response.data.data))
         setError(false)
-        if(checkinType === CHECK_IN) {
+        if (checkinType === CHECK_IN) {
           setLoading(false)
           dispatch(setActiveEmployee(null))
           setProcessing(false)
+          console.timeEnd('checkin')
         } else {
-          setTimeout(() => {
-            setLoading(false)
-            dispatch(setActiveEmployee(null))
-            setProcessing(false)
-          }, 4500);
+          setLoading(false)
+          dispatch(setActiveEmployee(null))
+          setProcessing(false)
+          console.timeEnd('checkin')
         }
       })
       .catch((error) => {
+        console.timeEnd('checkin')
         setError(true)
         setLoading(false)
       })
   }
 
   return (
-    <Transition.Root show as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={handleClose}>
+    <Transition.Root
+      show
+      as={Fragment}>
+      <Dialog
+        as='div'
+        className='relative z-10'
+        onClose={handleClose}>
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'>
+          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className='fixed inset-0 z-10 overflow-y-auto'>
+          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enterTo='opacity-100 translate-y-0 sm:scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'>
+              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
                 <div>
-                  <div className="flex items-center gap-4">
-                    { !error && checkinType === CHECK_IN && (
+                  <div className='flex items-center gap-4'>
+                    {!error && checkinType === CHECK_IN && (
                       <>
-                        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                          <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                        <div className='flex h-12 w-12 items-center justify-center rounded-full bg-green-100'>
+                          <CheckIcon
+                            className='h-6 w-6 text-green-600'
+                            aria-hidden='true'
+                          />
                         </div>
-                        <div className="text-2xl font-bold leading-6 text-gray-900">PRIJAVA</div>
+                        <div className='text-2xl font-bold leading-6 text-gray-900'>PRIJAVA</div>
                       </>
                     )}
-                    { !error && checkinType === CHECK_OUT && (
+                    {!error && checkinType === CHECK_OUT && (
                       <>
-                        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
-                          <XMarkIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                        <div className='flex h-12 w-12 items-center justify-center rounded-full bg-red-100'>
+                          <XMarkIcon
+                            className='h-6 w-6 text-red-600'
+                            aria-hidden='true'
+                          />
                         </div>
-                        <div className="text-2xl font-bold leading-6 text-gray-900">ODJAVA</div>
+                        <div className='text-2xl font-bold leading-6 text-gray-900'>ODJAVA</div>
                       </>
                     )}
-                    { !!error && (
-                      <div className="flex h-12 w-12 items-center justify-center rounded-full bg-orange-100">
-                        <ExclamationTriangleIcon className="h-6 w-6 text-orange-600" aria-hidden="true" />
+                    {!!error && (
+                      <div className='flex h-12 w-12 items-center justify-center rounded-full bg-orange-100'>
+                        <ExclamationTriangleIcon
+                          className='h-6 w-6 text-orange-600'
+                          aria-hidden='true'
+                        />
                       </div>
                     )}
                   </div>
-                  { !error && (
-                    <div className="mt-3 sm:mt-5">
-                      <Dialog.Title as="h3" className="text-2xl font-bold leading-6 text-gray-900">
-                        { activeEmployee.name }
+                  {!error && (
+                    <div className='mt-3 sm:mt-5'>
+                      <Dialog.Title
+                        as='h3'
+                        className='text-2xl font-bold leading-6 text-gray-900'>
+                        {activeEmployee.name}
                       </Dialog.Title>
-                      <div className="text-md mt-4 text-left">
-                        <p className="text-gray-500">
-                          Potvrdite { checkinType === CHECK_OUT ? ' odjavu.' : ' prijavu.' }
+                      <div className='text-md mt-4 text-left'>
+                        <p className='text-gray-500'>
+                          Potvrdite {checkinType === CHECK_OUT ? ' odjavu.' : ' prijavu.'}
                         </p>
-                        <div className="text-gray-500">
-                          U toku istog dana mozete se vise puta prijaviti i odjaviti.
-                        </div>
+                        <div className='text-gray-500'>U toku istog dana mozete se vise puta prijaviti i odjaviti.</div>
                       </div>
                     </div>
-                    )}
-                    { !!error && (
-                      <div className="mt-3 sm:mt-5">
-                      <Dialog.Title as="h3" className="text-2xl font-bold leading-6 text-gray-900">
+                  )}
+                  {!!error && (
+                    <div className='mt-3 sm:mt-5'>
+                      <Dialog.Title
+                        as='h3'
+                        className='text-2xl font-bold leading-6 text-gray-900'>
                         Došlo je do greške
                       </Dialog.Title>
-                      <div className="text-md mt-4 text-left">
-                        <p className="text-gray-500">
+                      <div className='text-md mt-4 text-left'>
+                        <p className='text-gray-500'>
                           Konekcija sa serverom nije moguća. Molimo sačekajte ili kontaktirajte nadredjenog.
                         </p>
                       </div>
                     </div>
-                    )}
+                  )}
                 </div>
-                <div className="mt-5 sm:mt-12 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                  <div className="sm:col-start-2">
-                    { !error && (
+                <div className='mt-5 sm:mt-12 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3'>
+                  <div className='sm:col-start-2'>
+                    {!error && (
                       <button
-                        type="button"
+                        type='button'
                         className={`relative inline-flex w-full justify-center rounded-md border border-transparent  px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-0 sm:text-sm
-                        ${checkinType === CHECK_OUT ? 'bg-red-600 hover:bg-red-700' : 'bg-indigo-600 hover:bg-indigo-700'}`}
-                        onClick={handleCheckin}
-                      >
-                        { checkinType === CHECK_IN && !processing && 'PRIJAVI SE' }
-                        { checkinType === CHECK_OUT && !processing && 'ODJAVI SE' }
-                        { processing && 'OBRADA...' }
-                        { processing && (
+                        ${
+                          checkinType === CHECK_OUT
+                            ? 'bg-red-600 hover:bg-red-700'
+                            : 'bg-indigo-600 hover:bg-indigo-700'
+                        }`}
+                        onClick={handleCheckin}>
+                        {checkinType === CHECK_IN && !processing && 'PRIJAVI SE'}
+                        {checkinType === CHECK_OUT && !processing && 'ODJAVI SE'}
+                        {processing && 'OBRADA...'}
+                        {processing && (
                           <div className='absolute top-1.5 right-2 w-6'>
-                            <svg x="0px" y="0px" className='w-full' viewBox="0 0 50 50">
-                              <path fill="#fff" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
-                                <animateTransform attributeType="xml"
-                                  attributeName="transform"
-                                  type="rotate"
-                                  from="0 25 25"
-                                  to="360 25 25"
-                                  dur="0.6s"
-                                  repeatCount="indefinite"/>
-                                </path>
-                              </svg>
+                            <svg
+                              x='0px'
+                              y='0px'
+                              className='w-full'
+                              viewBox='0 0 50 50'>
+                              <path
+                                fill='#fff'
+                                d='M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z'>
+                                <animateTransform
+                                  attributeType='xml'
+                                  attributeName='transform'
+                                  type='rotate'
+                                  from='0 25 25'
+                                  to='360 25 25'
+                                  dur='0.6s'
+                                  repeatCount='indefinite'
+                                />
+                              </path>
+                            </svg>
                           </div>
                         )}
                       </button>
                     )}
-                    {
-                      !processing && !!error && (
-                        <button
-                          type="button"
-                          className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-indigo-600 hover:bg-indigo-700 px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-0 sm:mt-0 sm:text-sm"
-                          onClick={() => window.location.reload()}
-                        >
-                          OSVEŽI STRANICU
-                        </button>
-                      )
-                    }
+                    {!processing && !!error && (
+                      <button
+                        type='button'
+                        className='inline-flex w-full justify-center rounded-md border border-gray-300 bg-indigo-600 hover:bg-indigo-700 px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-0 sm:mt-0 sm:text-sm'
+                        onClick={() => window.location.reload()}>
+                        OSVEŽI STRANICU
+                      </button>
+                    )}
                   </div>
-                  <div className="sm:col-start-1">
+                  <div className='sm:col-start-1'>
                     {!processing && (
                       <button
-                        type="button"
-                        className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-0 sm:col-start-1 sm:mt-0 sm:text-sm"
-                        onClick={handleClose}
-                      >
+                        type='button'
+                        className='mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-0 sm:col-start-1 sm:mt-0 sm:text-sm'
+                        onClick={handleClose}>
                         OTKAŽI
                       </button>
                     )}
